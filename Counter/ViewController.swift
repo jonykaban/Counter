96d00933215b91061719d92c85542e93f74607f9
jonykaban <nikita.fedorov124@gmail.com>
2026-02-25 10:45:56 +0300

    Базовое задание

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scoreLabel: UILabel!
+    private var score: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeScoreButton(_ sender: Any) {
+        score += 1
+        scoreLabel.text = "Значение счетчика: \(score)"
+    }
+    
 }
 
