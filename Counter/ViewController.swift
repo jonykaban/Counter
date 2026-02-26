6ab5807046b721788d6adb1fa0af1067afc98776
jonykaban <nikita.fedorov124@gmail.com>
2026-02-26 12:15:36 +0300

    Исправления усложненной задачи

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -7,10 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CounterViewController: UIViewController {
     
-    @IBOutlet weak var scoreLabel: UILabel!
-    @IBOutlet weak var historyTextView: UITextView!
+    @IBOutlet weak private var scoreLabel: UILabel!
+    @IBOutlet weak private var historyTextView: UITextView!
     private var score: Int = 0
     private var isFirstChange: Bool = true
     private let dateFormatter = DateFormatter()
@@ -21,22 +21,25 @@ class ViewController: UIViewController {
         
         dateFormatter.dateFormat = "dd-MM-YYYY HH-mm"
     }
-    @IBAction func plusButton(_ sender: Any) {
-        if isFirstChange {
-            historyTextView.text = ""
-            isFirstChange = false
+    
+    private func IsFirstChange() {
+        guard isFirstChange else {
+            return
         }
+        historyTextView.text = ""
+        isFirstChange = false
+    }
+    
+    @IBAction private func plusButton(_ sender: Any) {
+        IsFirstChange()
         
         score += 1
         scoreLabel.text = String(score)
         historyTextView.text += "\(dateFormatter.string(from: Date())): значение изменено на +1\n"
     }
     
-    @IBAction func minusButton(_ sender: Any) {
-        if isFirstChange {
-            historyTextView.text = ""
-            isFirstChange = false
-        }
+    @IBAction private func minusButton(_ sender: Any) {
+        IsFirstChange()
         
         if score <= 0 {
             historyTextView.text += "\(dateFormatter.string(from: Date())): попытка уменьшить значение счётчика ниже 0\n"
@@ -47,7 +50,7 @@ class ViewController: UIViewController {
             historyTextView.text += "\(dateFormatter.string(from: Date())): значение изменено на -1\n"
         }
     }
-    @IBAction func resetButton(_ sender: Any) {
+    @IBAction private func resetButton(_ sender: Any) {
         score = 0
         scoreLabel.text = "0"
         historyTextView.text += "\(dateFormatter.string(from: Date())): значение сброшено\n"
